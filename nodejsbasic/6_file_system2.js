a331fb02fc6e6dde57e9adde51df969bcfb3bcfb
Md Asad <126195098+asad710931@users.noreply.github.com>
2025-05-07 22:35:00 +0600

    Update 6_file_system2.js

--- nodejsbasic/6_file_system2.js
+++ nodejsbasic/6_file_system2.js
@@ -4,13 +4,13 @@ const gzip = zlib.createGzip();
 
 
 // createReadStream use for large files
-/*
+
 const readstream=fs.createReadStream('node.txt','utf8');
 const writestream=fs.createWriteStream('node1.txt');
 readstream.on('data',(data)=>{
     writestream.write(data);
 }) 
-*/
+
 const readstream=fs.createReadStream('test.txt','utf8');
 const writestream=fs.createWriteStream('test2.txt.rar');
 readstream.pipe(gzip).pipe(writestream);
